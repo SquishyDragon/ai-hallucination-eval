fa1df42e05bdbdfc606d8c43e620459caa4cac36
Nathan Treadaway <natetread@gmail.com>
2026-04-24 13:28:04 -0400

    changed prompt array to json dataset with ground truth and pass/fail conditioning

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from prompts import test_prompts
 from dotenv import load_dotenv
 from openai import OpenAI
 import time
+import json
 import os
 
 # Load API key
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-# Function to recieve AI response
+# Open prompts.json as file, then load JSON from file
+with open("prompts.json", "r") as file:
+    dataset = json.load(file)
+
+# Function to receive AI response
 def ask_ai(prompt):
-    #  Grab the response from OpenAI
+    # Grab the response from OpenAI
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
@@ -19,16 +23,20 @@ def ask_ai(prompt):
     )
     return response.choices[0].message.content
 
-# Create a visual serperator
+# Create a visual separator
 print("-" * 50)
 
-# Loop through the promtps stored in prompts.py
-for prompt in test_prompts:
+# Loop through the prompts stored in prompts.json
+for test_case in dataset:
+    # Set the prompt
+    prompt = test_case["prompt"]
     # Print the prompt
-    print("Prompt: " + prompt)
-    # print the response
-    print("Response: " + ask_ai(prompt))
-    # End the visual seperator
+    print(f"Prompt: {prompt}")
+    # Set the response
+    response = ask_ai(prompt)
+    # Print the response
+    print(f"Response: {response}")
+    # End the visual separator
     print("-" * 50)
     # Add a small delay (rate limiter)
     time.sleep(0.5)
